361feaf0b6ba15eda50590ca42db93f5c78cbbaf
Franklin She <franklinwshe@gmail.com>
2022-01-09 23:09:46 -0500

    Suppliers list and process template

--- src/components/Suppliers.js
+++ src/components/Suppliers.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import Card from "react-bootstrap/Card";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
+import Form from "react-bootstrap/Form";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
 
 class Suppliers extends Component {
   render() {
@@ -10,8 +13,67 @@ class Suppliers extends Component {
         <Card.Header>Sell</Card.Header>
         <Card.Body>
           <Tabs defaultActiveKey="list" id="sell-tab">
-            <Tab eventKey="list" title="List"></Tab>
-            <Tab eventKey="process" title="Process"></Tab>
+            <Tab eventKey="list" title="List">
+              <Form>
+                <Form.Group className="mb-3" controlId="formItemName">
+                  <Form.Label>Item Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="What are you selling?"
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formPrice">
+                  <Form.Label>Price (USD)</Form.Label>
+                  <Form.Control
+                    type="number"
+                    placeholder="How much do you want for it?"
+                  />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  List on Market
+                </Button>
+              </Form>
+            </Tab>
+            <Tab eventKey="process" title="Process">
+              Orders to be processed
+              <Table striped bordered hover size="sm">
+                <thead>
+                  <tr>
+                    <th>Order ID</th>
+                    <th>Customer</th>
+                    <th>Item Name</th>
+                    <th>Quantity</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Mark</td>
+                    <td>Coffee Beans</td>
+                    <td>2</td>
+                  </tr>
+                </tbody>
+              </Table>
+              Processed Orders
+              <Table striped bordered hover size="sm">
+                <thead>
+                  <tr>
+                    <th>Order ID</th>
+                    <th>Customer</th>
+                    <th>Item Name</th>
+                    <th>Quantity</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>2</td>
+                    <td>Joe</td>
+                    <td>Coffee Beans</td>
+                    <td>5</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Tab>
           </Tabs>
         </Card.Body>
       </Card>
